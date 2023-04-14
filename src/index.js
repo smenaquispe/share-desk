e32448d3d6f2bd1e4907a8b7872b4c6d20da381f
smenaq <sergiosantosmenaq@gmail.com>
2023-04-14 10:44:33 -0500

    connected with localhost:5000

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const http = require('http');
+app.use(cors(
+    {origin: 'http://localhost:5000'}
+))
 const server = http.createServer(app);
+
 
 const { Server } = require('socket.io')
 
-const io = new Server(server)
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5000', 
+        methods: ['GET', 'POST'], 
+        credentials: true 
+    }
+})
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    
-    //socket.broadcast.emit('hi')
     
     socket.on('disconnect', () => {
         console.log('user disconnected')
     })
 
     socket.on('chat message', msg => {
-        console.log(msg)
-        io.emit('chat message', msg)
+        console.log('message:', msg)
     })
+
+    socket.emit('header', { 'Access-Control-Allow-Origin': 'http://localhost:5000' });
 });
-  
+
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
